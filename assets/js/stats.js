5aca9278a8c72581bd1ccc662f22fa27fd7ba14e
yww009 <yww009@github>
2026-08-05 06:29:40 +0000

    feat: Plausible 统计 + jsDelivr CDN 镜像 + cdn-manifest.json

---
+++ assets/js/stats.js
@@ -0,0 +1,7 @@
+// Plausible Analytics - 零 cookie、隐私友好、免费
+(function(){
+  var s=document.createElement('script');
+  s.defer=true;s.dataset.domain='yww009.github.io';
+  s.src='https://plausible.io/js/script.js';
+  document.head.appendChild(s);
+})();
